35c639d2e5c8cadd408bde23fe69fc711ec249ec
Emmanuel <okoyee61@gmail.com>
2026-02-23 10:05:36 +0100

    added url

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,11 @@
 // prisma/prisma.config.ts
+import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
-// Load .env before defining the config
-import "dotenv/config"; 
-// or you can use the dotenv config function:
-// import { config } from "dotenv";
-// config();
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  // ... other configurations
   datasource: {
-    url: env("DATABASE_URL"), // Use env() helper for type safety or process.env.DATABASE_URL
+    // We use DIRECT_URL here so Prisma CLI can always connect during builds
+    url: env("DIRECT_URL"), 
   },
 });
